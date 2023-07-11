cbe5c833fa25bcd91a9e817a4b08d681f1e8c98c
Felipegamboagaga <Feligamboaga@gmail.com>
2023-07-10 23:54:05 -0500

    retocamos los valores del gradiante rgba

--- JS/main.js
+++ JS/main.js
@@ -2,7 +2,9 @@ let loginbod = document.querySelector('.loginbod');
 loginbod.addEventListener('mousemove', function(e){
     x = e.offsetX;
     y = e.offsetY;
-    loginbod.style.background  =`linear-gradient(45deg, rgba(${x/3},0,0), rgba(0,0,${y/3})`
+    c = Math.sqrt(x * x + y * y);
+    loginbod.style.background  =`linear-gradient(45deg, rgba(${x/10},${c},${y/10}), rgba(0,0,255)`
+    document.getElementById('textosuperior').innerHTML = x+"    "+y;
 
 });
 
