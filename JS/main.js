7809b539d3a5995389fc8c7ff4f80791631ceb02
Felipegamboagaga <Feligamboaga@gmail.com>
2023-07-11 00:40:57 -0500

    opcion de registro

--- JS/main.js
+++ JS/main.js
@@ -7,6 +7,26 @@ loginbod.addEventListener('mousemove', function(e){
     document.getElementById('textosuperior').innerHTML = x+"    "+y;
 
 });
+const registerbox = document.querySelector('.registerbox');
+const loginbox = document.querySelector('.loginbox');
+const wrapper = document.querySelector('.wrapper');
+const Loglink = document.querySelector('.linklogin');
+const Reglink = document.querySelector('.linkregistro');
+console.log(registerbox, loginbox, wrapper)
+Reglink.addEventListener('click',()=>{
+    wrapper.classList.add('activo');
+    wrapper.style.height = '520px';
+    registerbox.style.transform = 'translateX(0)';
+    loginbox.style.transform = 'translateX(-400px)';
 
 
+})
+Loglink.addEventListener('click',()=>{
+    wrapper.classList.remove('activo');
+    wrapper.style.height = '440px'
+    loginbox.style.transform = 'translateX(0)';
+    registerbox.style.transform = 'translateX(400px)';
+
+
+})
 
